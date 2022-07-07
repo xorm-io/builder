d74b6f105080fb3ba7db8daac887570621a58be9
kolaente <kolaente@noreply.gitea.io>
2022-07-07 08:18:02 +0800

    Improve IN and NOT IN queries (#76)
    Reviewed-on: https://gitea.com/xorm/builder/pulls/76
    Co-authored-by: kolaente <kolaente@noreply.gitea.io>
    Co-committed-by: kolaente <kolaente@noreply.gitea.io>

--- cond_test.go
+++ cond_test.go
@@ -4,8 +4,65 @@
 
 package builder
 
-import "testing"
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestCond_NotIn(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		cond := NotIn("test", 1, 2, 2, 3, 4, 4, 4)
+		buf := NewWriter()
+		err := cond.WriteTo(buf)
+		assert.NoError(t, err)
+		assert.Equal(t, "test NOT IN (?,?,?,?)", buf.String())
+		assert.Len(t, buf.args, 4)
+		assert.Equal(t, []interface{}{1, 2, 3, 4}, buf.args)
+	})
+	t.Run("slice", func(t *testing.T) {
+		cond := NotIn("test", []int{1, 2, 2, 3, 4, 4, 4})
+		buf := NewWriter()
+		err := cond.WriteTo(buf)
+		assert.NoError(t, err)
+		assert.Equal(t, "test NOT IN (?,?,?,?)", buf.String())
+		assert.Len(t, buf.args, 4)
+		assert.Equal(t, []interface{}{1, 2, 3, 4}, buf.args)
+	})
+	t.Run("blank", func(t *testing.T) {
+		cond := NotIn("test")
+		buf := NewWriter()
+		err := cond.WriteTo(buf)
+		// The "error" is written to the bytes buffer
+		assert.NoError(t, err)
+		assert.Equal(t, "0=0", buf.String())
+	})
+}
 
+func TestCond_In(t *testing.T) {
+	t.Run("normal", func(t *testing.T) {
+		cond := In("test", 1, 2, 2, 3, 4, 4, 4)
+		buf := NewWriter()
+		err := cond.WriteTo(buf)
+		assert.NoError(t, err)
+		assert.Equal(t, "test IN (?,?,?,?)", buf.String())
+		assert.Len(t, buf.args, 4)
+		assert.Equal(t, []interface{}{1, 2, 3, 4}, buf.args)
+	})
+	t.Run("slice", func(t *testing.T) {
+		cond := In("test", []int{1, 2, 2, 3, 4, 4, 4})
+		buf := NewWriter()
+		err := cond.WriteTo(buf)
+		assert.NoError(t, err)
+		assert.Equal(t, "test IN (?,?,?,?)", buf.String())
+		assert.Len(t, buf.args, 4)
+		assert.Equal(t, []interface{}{1, 2, 3, 4}, buf.args)
+	})
+	t.Run("blank", func(t *testing.T) {
+		cond := In("test")
+		buf := NewWriter()
+		err := cond.WriteTo(buf)
+		// The "error" is written to the bytes buffer
+		assert.NoError(t, err)
+		assert.Equal(t, "0=1", buf.String())
+	})
 }
